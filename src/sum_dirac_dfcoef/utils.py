23cee3e3c795738ba29960284c67a9ea32b5572e
Kohei Noda <kohei-noda@hiroshima-u.ac.jp>
2023-11-26 20:16:01 +0900

    Rename from str to message
    Redefining built-in 'str' Pylint (W0622:redefined-builtin)

--- src/sum_dirac_dfcoef/utils.py
+++ src/sum_dirac_dfcoef/utils.py
@@ -5,12 +5,12 @@ def space_separated_parsing(line: str) -> "list[str]":
     return [word for word in words if word != ""]
 
 
-def debug_print(str: str):
+def debug_print(message: str):
     from .args import args
 
     # print debug message if --debug option is used
     if args.debug:
-        print(str)
+        print(message)
 
 def is_float(parameter: str):
     if not parameter.isdecimal():
