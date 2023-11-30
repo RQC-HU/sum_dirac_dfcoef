9c036a0ad6b3d6c5faf0194df2b3cc8c7f8d4e2d
Kohei Noda <ponserviceregist@gmail.com>
2023-11-30 17:01:20 +0900

    Fix PrintVersionExitAction help parameter name

--- src/sum_dirac_dfcoef/args.py
+++ src/sum_dirac_dfcoef/args.py
@@ -3,13 +3,13 @@ import sys
 
 
 class PrintVersionExitAction(argparse.Action):
-    def __init__(self, option_strings, dest=argparse.SUPPRESS, default=argparse.SUPPRESS, help=None):
+    def __init__(self, option_strings, dest=argparse.SUPPRESS, default=argparse.SUPPRESS, help_=None):
         super().__init__(
             option_strings=option_strings,
             dest=dest,
             default=default,
             nargs=0,
-            help=help,
+            help=help_,
         )
 
     def __call__(self):
