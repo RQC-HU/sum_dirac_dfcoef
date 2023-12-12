7fcd9eb439bbef2bb2f9200ee569e9cc8486dac4
Kohei Noda <ponserviceregist@gmail.com>
2023-12-12 21:16:13 +0900

    Add dirac input handling functions

--- src/sum_dirac_dfcoef/utils.py
+++ src/sum_dirac_dfcoef/utils.py
@@ -1,9 +1,8 @@
+import re
 from typing import List
 
 
 def space_separated_parsing(line: str) -> List[str]:
-    import re
-
     words = re.split(" +", line.rstrip("\n"))
     return [word for word in words if word != ""]
 
@@ -25,3 +24,31 @@ def is_float(parameter: str):
             return False
     else:
         return False
+
+
+def is_dirac_input_section(word: str) -> bool:
+    regex_section = r" *\*{1,2}[0-9A-Z]+"
+    return re.match(regex_section, word) is not None
+
+
+def is_dirac_input_section_one_star(word: str) -> bool:
+    regex_section = r" *\*[0-9A-Z]+"
+    return re.match(regex_section, word) is not None
+
+
+def is_dirac_input_section_two_stars(word: str) -> bool:
+    regex_section = r" *\*{2}[0-9A-Z]+"
+    return re.match(regex_section, word) is not None
+
+
+def is_dirac_input_line_comment_out(word: str) -> bool:
+    regex_comment_out = r" *[!#]"
+    return re.match(regex_comment_out, word) is not None
+
+
+def delete_comment_out(line: str) -> str:
+    regex_comment_out = r" *[!#]"
+    idx_comment_out = re.search(regex_comment_out, line)
+    if idx_comment_out is None:
+        return line
+    return line[: idx_comment_out.start()]
