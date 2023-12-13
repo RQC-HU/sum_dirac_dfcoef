855c03ba248ce72608010d23313cd2229fa84ef5
Kohei Noda <ponserviceregist@gmail.com>
2023-12-14 01:33:01 +0900

    Stop reading output at the end of the input section.

--- src/sum_dirac_dfcoef/moltra.py
+++ src/sum_dirac_dfcoef/moltra.py
@@ -34,26 +34,36 @@ class MoltraInfo:
 
         is_moltra_section = False
         re_active_keyword = r" *\.ACTIVE"
+        is_reach_input_field = False
         is_next_line_active = False
         for line in dirac_output:
             words = [word.upper() for word in space_separated_parsing(line)]
             if len(words) == 0 or is_dirac_input_line_comment_out(words[0]):
                 continue
+            # If we reach this line, it means that we are in the input field
+            if "Contents of the input file" in line:
+                is_reach_input_field = True
+                continue
 
-            if is_dirac_input_section_two_stars(words[0]):
-                if "**MOLTRA" in words[0]:
-                    is_moltra_section = True
-                    continue
+            # If we reach this line, it means the end of the input field
+            if "Contents of the molecule file" in line:
+                break  # end of input field
 
-            if is_moltra_section:
-                if re.match(re_active_keyword, line) is not None:
-                    cls.is_default = False
-                    is_next_line_active = True
-                    continue
+            if is_reach_input_field:
+                if is_dirac_input_section_two_stars(words[0]):
+                    if "**MOLTRA" in words[0]:
+                        is_moltra_section = True
+                        continue
 
-            if is_next_line_active:
-                if is_dirac_input_section(words[0]) or is_dirac_input_keyword(words[0]):
-                    # End of the .ACTIVE section
-                    break
-                no_comment_line = delete_comment_out(line)
-                cls.range_str.append(no_comment_line.strip())
+                if is_moltra_section:
+                    if re.match(re_active_keyword, line) is not None:
+                        cls.is_default = False
+                        is_next_line_active = True
+                        continue
+
+                if is_next_line_active:
+                    if is_dirac_input_section(words[0]) or is_dirac_input_keyword(words[0]):
+                        # End of the .ACTIVE section
+                        break
+                    no_comment_line = delete_comment_out(line)
+                    cls.range_str.append(no_comment_line.strip())
