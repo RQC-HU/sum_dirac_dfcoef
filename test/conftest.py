2c8af0b612d745ce20300d528896322eb2e88774
Kohei Noda <ponserviceregist@gmail.com>
2024-01-10 22:37:14 +0900

    Add optional dependencies for development and testing

--- test/conftest.py
+++ test/conftest.py
@@ -8,7 +8,7 @@ def pytest_sessionstart():
     # Change the current directory to the root directory of the package
     test_dir = Path(__file__).resolve().parent
     os.chdir(Path.joinpath(test_dir, ".."))
-    cmd = "python3 -m pip install -e ."
+    cmd = "python3 -m pip install -e .[test]"
     subprocess.run(cmd.split(), check=True)
     # Change the current directory to the original directory
     os.chdir(cur_dir)
