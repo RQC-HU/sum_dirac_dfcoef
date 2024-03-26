47c3c57ebb0dfcd228964f894d6d4e1a1943d8a4
Kohei Noda <ponserviceregist@gmail.com>
2024-03-26 22:53:39 +0900

    Remove *.out files in the results directory at the beginning of test session.

--- test/conftest.py
+++ test/conftest.py
@@ -7,6 +7,11 @@ def pytest_sessionstart():
     cur_dir = Path.cwd()
     # Change the current directory to the root directory of the package
     test_dir = Path(__file__).resolve().parent
+    results_dir = Path.joinpath(test_dir, "results")
+    if results_dir.exists():
+        # Remove *.out files in the results directory
+        for file in results_dir.glob("*.out"):
+            os.remove(file)
     os.chdir(Path.joinpath(test_dir, ".."))
     cmd = "python3 -m pip install -e .[test]"
     subprocess.run(cmd.split(), check=True)
